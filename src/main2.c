8e543ce270bd56de278790c5b03f0d5b48ede6ac
Louis Galloux <lgalloux@c2r8p2.42lehavre.fr>
2024-09-11 14:54:10 +0200

    add new main + print test

---
+++ src/main2.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main2.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgalloux <lgalloux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 16:03:23 by lgalloux          #+#    #+#             */
+/*   Updated: 2024/09/11 14:53:24 by lgalloux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	t_pipex		pipex;
+
+	if (argc < 5)
+		ft_error("wrong number of arguments\n", &pipex, -WRITE_MSG, 1);
+	pipex.here_doc = 0;
+	here_doc_verif(&pipex, argc, argv);
+	struct_init(&pipex, argv, argc, env);
+	init_pipe_fds(&pipex);
+	parent(argv, argc - 2, &pipex);
+	free_all(&pipex, FREE_LST);
+	ft_printf("test");
+	return (0);
+}
